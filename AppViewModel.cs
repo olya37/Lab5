4d89b085e37c2c40704b22b4e47f6abf91b5ea59
olya37 <156088135+olya37@users.noreply.github.com>
2024-01-10 14:00:12 +0300

    Add files via upload

---
+++ AppViewModel.cs
@@ -0,0 +1,37 @@
+﻿using SimpleContactBook.Services;
+using SimpleContactBook.Utility;
+using SimpleContactBook.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleContactBook
+{
+    public class AppViewModel : ObservableObject
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { OnPropertyChanged(ref _currentView, value); }
+        }
+
+        private BookViewModel _bookVM;
+        public BookViewModel BookVM 
+        { 
+            get { return _bookVM; } 
+            set { OnPropertyChanged(ref _bookVM, value); }
+        }
+
+        public AppViewModel()
+        {
+            var dataService = new MockDataService();
+
+            BookVM = new BookViewModel(dataService);
+            CurrentView = BookVM;
+        }
+
+    }
+}
